805ff7b8c703a31c151b6bad37d88893b379875e
xtruong <truongxuanha882@gmail.com>
2024-08-09 16:48:10 +0700

    banner fix

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -27,6 +27,7 @@ const Banner: React.FC = () => {
     const fetchBanners = async () => {
       try {
         const { data } = await axiosIntance.get("/banner");
+        if (data.status === false) throw Error("Error");
         setBanners(data.data);
       } catch (err) {
         console.error("Error fetching data:", err);
