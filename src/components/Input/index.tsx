a1712d49f0295498e960af4ac2a42fee69cd39e4
xtruong <truongxuanha882@gmail.com>
2024-08-11 15:19:48 +0700

    update

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,11 @@
 import { InputProps } from "../../types/AllType.type";
 
-function Input({ placeholder, type, value, onChange }: InputProps) {
+function Input({ placeholder, type, value, onChange, className }: InputProps) {
   return (
     <input
       placeholder={placeholder}
       type={type}
-      className={`input-global ${type}`}
+      className={`input-global ${className}`}
       value={value}
       onChange={onChange}
     />
