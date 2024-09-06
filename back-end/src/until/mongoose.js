f27c241703033e6a4a236a764dc9ebef02683f41
truongxuanha <truongxuanha882@gmail.com>
2024-09-06 11:21:07 +0700

    add admin

--- back-end/src/until/mongoose.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mutipleMongooseToObject: function (mongooseArray){
-        return mongooseArray.map(mongoose => mongoose.toObject())
-    },
-    mongooseToObject: function (mongoose){
-        return mongoose ? mongoose.toObject() : mongoose
-    }
-}
