3d66068a2cf1ac45725bb668ed07089010726c1e
truongxuanha <truongxuanha882@gmail.com>
2024-12-29 13:18:43 +0700

    feature/update_profile

--- src/components/Profile/MeAccountPage/index.tsx
+++ src/components/Profile/MeAccountPage/index.tsx
@@ -10,7 +10,6 @@ import LoadingMini from "customs/LoadingMini";
 
 const MeAccountPage = () => {
   const { infoUser, loadingChangeProfile } = useAppSelector((state) => state.auth);
-  const { account_name, email, phone } = infoUser;
   const dispatch = useAppDispatch();
   const { register, handleSubmit } = useForm<any>({
     resolver: yupResolver(schemaChangeProfile),
@@ -25,8 +24,10 @@ const MeAccountPage = () => {
   });
 
   const onSubmit = async (formValue: UserAccount) => {
-    dispatch(authChangeProfileThunk({ ...formValue }));
-    await dispatch(authProfle());
+    const callBack = () => {
+      dispatch(authProfle());
+    };
+    dispatch(authChangeProfileThunk({ ...formValue, callBack }));
   };
   return (
     <div className="bg-white p-6">
@@ -35,59 +36,48 @@ const MeAccountPage = () => {
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.NAME_LOGIN}</label>
           <input
+            id="account_name"
             {...register("account_name")}
             type="text"
-            value={account_name}
             className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </div>
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.FULL_NAME}</label>
-          <input {...register("full_name")} type="text" className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500" />
+          <input
+            id="full_name"
+            {...register("full_name")}
+            type="text"
+            className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
+          />
         </div>
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.PHONE_NUMBER}</label>
           <input
+            id="phone"
             {...register("phone")}
             type="text"
-            value={phone}
             className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
           />
         </div>
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.EMAIL}</label>
-          <input
-            {...register("email")}
-            type="text"
-            value={email}
-            className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
-          />
+          <input {...register("email")} type="text" className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500" />
         </div>
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.SEX}</label>
           <div className="w-2/4 flex space-x-4">
-            <label className="flex items-center">
-              <input value={0} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 0} />
-              {SEX.MALE}
-            </label>
-            <label className="flex items-center">
-              <input value={1} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 1} />
-              {SEX.FEMALE}
-            </label>
-            <label className="flex items-center">
-              <input value={3} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 3} />
-              {SEX.OTHER}
-            </label>
+            <label>{SEX.MALE}</label>
+            <input value={0} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 0} />
+            <label>{SEX.FEMALE}</label>
+            <input value={1} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 1} />
+            <label>{SEX.OTHER}</label>
+            <input value={3} {...register("sex")} type="radio" name="sex" className="mr-2" defaultChecked={infoUser.sex === 3} />
           </div>
         </div>
         <div className="flex items-center">
           <label className="w-1/4">{texts.account.BIRTHDAY}</label>
-          <input
-            {...register("birthday")}
-            type="date"
-            placeholder="mm/dd/yyyy"
-            className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500"
-          />
+          <input {...register("birthday")} type="date" className="w-2/4 border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500" />
         </div>
         <button
           type="submit"
